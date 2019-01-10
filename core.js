4816db7407eeef8184eca3cc6850611ec4e4cdd4
jdk <zhe.dan28@gmail.com>
2019-01-10 15:30:52 +0500

    Added pay, create invoice operations

--- core.js
+++ core.js
@@ -1,8 +1,15 @@
-function sendPostRequest(url, formData, resolve = null, reject = null) {
+function sendPostRequest(url, formData, resolve = null, reject = null, headers={}) {
     let xhr = new XMLHttpRequest();
 
     xhr.open('POST', url, true);
     xhr.withCredentials = true;
+
+    for (let key in headers) {
+        if (headers.hasOwnProperty(key)) {
+            xhr.setRequestHeader(key, headers[key]);
+        }
+    }
+
     xhr.onreadystatechange = function () {
         handleRequest(this, resolve, reject)
     };
@@ -27,12 +34,40 @@ function handleRequest(req, resolve = null, reject = null) {
 
     if (req.status !== 200) {
         if (reject != null) {
-            reject();
+            reject(req.response);
         }
         return;
     }
 
     if (resolve != null) {
-        resolve(req.responseText);
+        resolve(req.response);
+    }
+}
+
+function addAlert(el, html) {
+    let newElement = createElementFromHTML(html);
+    el.prepend(newElement);
+
+    setTimeout(function () {
+        newElement.parentNode.removeChild(newElement);
+    }, 3000)
+}
+
+//https://stackoverflow.com/a/494348
+function createElementFromHTML(htmlString) {
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+
+    // Change this to div.childNodes to support multiple top-level nodes
+    return div.firstChild;
+}
+
+function convertFormToJson(form) {
+    if (form instanceof FormData) {
+        let object = {};
+
+        form.forEach((value, key) => object[key] = value);
+
+        return object;
     }
 }
